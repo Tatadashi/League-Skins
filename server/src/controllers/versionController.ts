697681485c78c98a0309713686b7b2144b55d3c2
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-10-28 05:37:02 -0700

    Changing version, also should refetch skins
    hopefully

--- server/src/controllers/versionController.ts
+++ server/src/controllers/versionController.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 
 export async function versionGet(req: Request, res: Response) {
-  res.send("Patch 25.21");
+  //e for edits on my side not Rito
+  res.send("Patch 25.21 e1");
 }
