a3ef2219fa3328ed231093ac73996f9bfc1c5bfd
Kenny Luu <85083572+Tatadashi@users.noreply.github.com>
2025-10-28 00:55:43 -0700

    Finish about and basically ready for host

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # League-Skins
 
+I made this so that my friends and I could view League skins
+without having to login to the client. With League being my forever and favorite game, being able to incorporate my two main interests was really nice and fun. It turned out a lot better
+than I thought and I learned a lot making it. I still would like
+to add more features to it in the future and obviously keep it up
+to date per latest patch.
+
 "League Skins" isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.
 
-Passion Project meant to help players keep track of their favorite skins and when they go on sale all in one place. For questions, please contact us via Kenny’s Linkedin inbox:
-
-# Credits 
+# Credits
 
 Programmer: Kenny Luu
 Art and UI Design: Thanh Nguyen
@@ -14,34 +18,26 @@ Icons:
 github: https://devicon.dev/
 title sparkle: https://www.rawpixel.com/image/6194095/png-aesthetic-sticker
 user: Helmet Bro Summoner Icon - https://wiki.leagueoflegends.com/en-us/File:Helmet_Bro_profileicon.png
-rarity gems: 
-    - standard (rare) - https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-collections/global/default/images/gems/rare.png
-    - all other rarities (at the bottom) - https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/rarity-gem-icons/
-other: https://uxwing.com/license/
-    - light mode - https://uxwing.com/day-sunny-icon/
-    - dark mode - https://uxwing.com/moon-line-icon/
-    - home - https://uxwing.com/instagram-home-feed-icon/
-    - sale - https://uxwing.com/tag-line-icon/
-    - favorite - https://uxwing.com/heart-thin-icon/
-    - unfavorite - https://uxwing.com/broken-heart-icon/
-    - collection - https://uxwing.com/list-round-bullet-icon/
-    - about - https://uxwing.com/list-round-bullet-icon/
-    - search - https://uxwing.com/search-line-icon/
+rarity gems: - standard (rare) - https://raw.communitydragon.org/latest/plugins/rcp-fe-lol-collections/global/default/images/gems/rare.png - all other rarities (at the bottom) - https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/rarity-gem-icons/
+other: https://uxwing.com/license/ - light mode - https://uxwing.com/day-sunny-icon/ - dark mode - https://uxwing.com/moon-line-icon/ - home - https://uxwing.com/instagram-home-feed-icon/ - sale - https://uxwing.com/tag-line-icon/ - favorite - https://uxwing.com/heart-thin-icon/ - unfavorite - https://uxwing.com/broken-heart-icon/ - collection - https://uxwing.com/list-round-bullet-icon/ - about - https://uxwing.com/list-round-bullet-icon/ - search - https://uxwing.com/search-line-icon/
 
 All other art from Riot fetched from CommunityDragon Raws
 
 Thank you to SkinSpotlights and Khada, support them below
- - https://modelviewer.lol/ - https://ko-fi.com/khada3d
- - https://www.youtube.com/channel/UC0NwzCHb8Fg89eTB5eYX17Q - Epic Creator Code: skinspotlights
+
+- https://modelviewer.lol/ - https://ko-fi.com/khada3d
+- https://www.youtube.com/channel/UC0NwzCHb8Fg89eTB5eYX17Q - Epic Creator Code: skinspotlights
 
 Thanks to the Riot Games Community (Wiki, CommunityDragon, 3rd Party Devs)
- - https://wiki.leagueoflegends.com/en-us/ - https://discord.com/invite/zEe6fcw6fm
- - https://www.communitydragon.org/ - https://www.patreon.com/communitydragon - https://discord.com/invite/rZQwuek
- - https://discord.com/invite/riotgamesdevrel
+
+- https://wiki.leagueoflegends.com/en-us/ - https://discord.com/invite/zEe6fcw6fm
+- https://www.communitydragon.org/ - https://www.patreon.com/communitydragon - https://discord.com/invite/rZQwuek
+- https://discord.com/invite/riotgamesdevrel
 
 Roboto (sidebar/about): https://fonts.google.com/specimen/Roboto
 
 # Roadmap
+
 - champion page should have skins
 - filter skinlines
 - sort alphabetical, release, champion (alphabetical), tier
@@ -50,7 +46,7 @@ Roboto (sidebar/about): https://fonts.google.com/specimen/Roboto
 - if logins work, email (validate) when sale (prob use db)
 - tests front/back
 - more sorting categories (classes, roles, regions)
-- use cloudflare r2 to host images (not sure how much, hopefully free) the cache images too (this is so not rely on CDD for images) 
+- use cloudflare r2 to host images (not sure how much, hopefully free) the cache images too (this is so not rely on CDD for images)
 - weekly sales includes hextech rotation (maybe super limited stuff like that jax skin)
 - newest skins + patch
 - include pbe (upcoming skins, pbe tag) or have page for that
--- client/src/app/pages/about/about.tsx
+++ client/src/app/pages/about/about.tsx
@@ -11,8 +11,164 @@ export default function About() {
       <Title />
       <div className="page-layout">
         <Sidebar />
-        <div>
-          <h1>About</h1>
+        <div className="flex items-center flex-col gap-5 sm:gap-10 md:gap-20 lg:gap-30 p-5 md:p-20 text-sm md:text-md text-center">
+          <div className="text-gray-600 dark:text-gray-400">
+            <p>
+              I made this so that my friends and I could view League skins
+              without having to login to the client. With League being my
+              forever and favorite game, being able to incorporate my two main
+              interests was really nice and fun. It turned out a lot better than
+              I thought and I learned a lot making it. I still would like to add
+              more features to it in the future and obviously keep it up to date
+              per latest patch.
+            </p>
+            <br />
+            <p>
+              It is a JamStack PERN project built with TypeScript, styled with
+              TailwindCSS, and formatted using ESLint and Prettier. The data is
+              parsed from CommunityDragon and the Official League Wiki.
+            </p>
+          </div>
+          <div className="font-bold">
+            <h1>Credits:</h1>
+            <h2>
+              Programmer (Front & Back End, DB, Hosting) and Creator:{" "}
+              <a
+                className="link-about"
+                href="https://www.linkedin.com/in/kenny-luu-kl/"
+                aria-label="Kenny Luu's LinkedIn"
+              >
+                Kenny Luu
+              </a>
+            </h2>
+            <h2>
+              Art (Aatrox Sword and Neeko) and UI/UX:{" "}
+              <a
+                className="link-about"
+                href="https://www.linkedin.com/in/tnn-thanh-nguyen/"
+                aria-label="Thanh Nguyen's LinkedIn"
+              >
+                Thanh Nguyen
+              </a>
+            </h2>
+            <br />
+            <div className="font-normal">
+              <h3>
+                Most of icons from{" "}
+                <a
+                  className="link-about"
+                  href="https://uxwing.com/"
+                  aria-label="UXWing Website"
+                >
+                  UXWing
+                </a>{" "}
+                and{" "}
+                <a
+                  className="link-about"
+                  href="https://devicon.dev/"
+                  aria-label="Devicon Website"
+                >
+                  Devicon
+                </a>{" "}
+              </h3>
+              <h3>
+                Most data from{" "}
+                <a
+                  className="link-about"
+                  href="https://www.communitydragon.org/"
+                  aria-label="CommunityDragon Website"
+                >
+                  Community Dragon
+                </a>{" "}
+                Raws, please support them{" "}
+                <a
+                  className="link-about"
+                  href="https://www.patreon.com/communitydragon"
+                  aria-label="CommunityDragon Patreon"
+                >
+                  here
+                </a>{" "}
+              </h3>
+              <h3>
+                Helpful Discord Servers:{" "}
+                <a
+                  className="link-about"
+                  href="https://discord.com/invite/rZQwuek"
+                  aria-label="CommunityDragon Discord"
+                >
+                  Community Dragon
+                </a>{" "}
+                ,{" "}
+                <a
+                  className="link-about"
+                  href="https://discord.com/invite/zEe6fcw6fm"
+                  aria-label="Riot Wiki Discord"
+                >
+                  Wiki
+                </a>{" "}
+                ,{" "}
+                <a
+                  className="link-about"
+                  href="https://discord.com/invite/riotgamesdevrel"
+                  aria-label="Riot 3rd Party Developer Discord"
+                >
+                  Riot 3rd Party Developer
+                </a>{" "}
+              </h3>
+              <h3>
+                Official{" "}
+                <a
+                  className="link-about"
+                  href="https://wiki.leagueoflegends.com/en-us/"
+                  aria-label="Riot Wiki Website"
+                >
+                  Wiki
+                </a>{" "}
+                is an amazing resource
+              </h3>
+              <h3>
+                Thanks to these cool creators:{" "}
+                <a
+                  className="link-about"
+                  href="https://www.youtube.com/channel/UC0NwzCHb8Fg89eTB5eYX17Q"
+                  aria-label="Skin Spotlights YT Channel"
+                >
+                  Skin Spotlights
+                </a>{" "}
+                and{" "}
+                <a
+                  className="link-about"
+                  href="https://modelviewer.lol/"
+                  aria-label="Khada Website"
+                >
+                  Khada
+                </a>{" "}
+              </h3>
+              <h4>
+                More Comprehensive info on{" "}
+                <a
+                  className="link-about"
+                  href="https://github.com/Tatadashi/League-Skins"
+                  aria-label="GitHub Repo"
+                >
+                  GitHub
+                </a>{" "}
+              </h4>
+            </div>
+          </div>
+          <div>
+            <h2>
+              For any questions or suggestions, contact via{" "}
+              <a
+                className="link-about"
+                href="https://www.linkedin.com/in/kenny-luu-kl/"
+                aria-label="Kenny Luu's LinkedIn"
+              >
+                Kenny's LinkedIn
+              </a>{" "}
+              inbox
+            </h2>
+          </div>
         </div>
         <Banner />
       </div>
